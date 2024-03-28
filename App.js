17e68571fee2ed2f5e6773329d8a2c1611bd9b61
Yo <yoannbliti@yahoo.fr>
2024-03-28 16:57:57 +0100

    mise a jour de la calculatrice

--- App.js
+++ App.js
@@ -1,49 +1,12 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import Calculette from './Calculette';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [input, setInput] = useState('');
-
-  const handleClick = (value) => {
-    setInput(input + value);
-  };
-
-  const calculateResult = () => {
-    try {
-      const result = eval(input);
-      setInput(result.toString());
-    } catch (error) {
-      setInput('Error');
-    }
-  };
-
-  const clearInput = () => {
-    setInput('');
-  };
-
   return (
-    <div className="calculator">
-      <input type="text" value={input} readOnly />
-      <div className="buttons">
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('+')}>+</button>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('-')}>-</button>
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('*')}>*</button>
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('.')}>.</button>
-        <button onClick={clearInput}>C</button>
-        <button onClick={() => handleClick('/')}>/</button>
-        <button onClick={calculateResult}>=</button>
-      </div>
-    </div>
+    <Calculette/>
+
   );
 }
 
